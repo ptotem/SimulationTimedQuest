c8ceb1528eb95ec9a5dbe50de891bfbd47d2bbd8
rakesh19487 <rakesh.rengaraj@gmail.com>
2015-07-07 18:52:50 +0530

    minor changes

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   get 'home/index'
   mount RailsAdmin::Engine => '/admin', :as => 'rails_admin'
   mount RailsAdminImport::Engine => '/rails_admin_import', :as => 'rails_admin_import'
-  
+
   devise_for :users
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
